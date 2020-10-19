e6e8d85720548a2603d0ccd33632537df5a3d02b
Philipp Oeschger <philipp.oeschger@hs-furtwangen.de>
2020-10-19 17:47:52 +0200

    Übung 3

--- Praktikum/src/Aufgabe3/Textanalyse.java
+++ Praktikum/src/Aufgabe3/Textanalyse.java
@@ -1,2 +1,29 @@
-package Aufgabe3;public class Textanalyse {
+package Aufgabe3;
+
+public class Textanalyse {
+
+    public static void main(String[] args) {
+        int[] counterArray = new int[26];
+        String abcLookup = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        System.out.println(args.length + " Argumente übergeben");
+
+        //Wort Analyse
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("Länge Aurgument " + i + ": " + args[i].length());
+
+            // Buchtaben Analyse
+            for (int y = 0; y < args[i].length(); y++) {
+                char letter = Character.toUpperCase((args[i].charAt(y)));
+                int index = abcLookup.indexOf(letter);
+                ++counterArray[index];
+            }
+        }
+
+        System.out.println("\nBuchstabenanzahlen: ");
+        for (int i = 0; i < counterArray.length; i++) {
+            if (counterArray[i] != 0) {
+                System.out.println(abcLookup.charAt(i) + ": " + counterArray[i]);
+            }
+        }
+    }
 }
